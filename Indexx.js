91c5c5a0ba3af8f944ab629cd4687b0e04534b0b
UMAR-FAROUK BASHIR <UMAR@test-vm2.local.layer3.ng>
2021-08-24 16:29:57 +0100

    first commit

---
+++ Indexx.js
@@ -0,0 +1,29 @@
+
+  class Bashir {
+    constructor(first, middle, last) {
+        this.first = first;
+        this.middle = middle;
+        this.last = last;
+    }
+
+     getFullName() {
+        return this.first + " " + this.middle + " " +this.last;
+    }
+
+}
+
+
+
+
+//console.log(Umar.getFullName());
+
+//console.log(Halimah.getFullName());
+
+
+
+
+
+
+
+
+
---
+++ Project.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: black;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 17px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: black;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: black;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="Index.html">Home</a>
+  <a href="https://www.adultswim.com/etcetera/elastic-man/">Contact us</a>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">About us</a>
+</div>
+
+<div style="padding-left:11px">
+  <h2>Business Name</h2>
+  <p>Services Provided, Pricing, Working Day .etc</p>
+</div>
+
+</body>
+</html>
